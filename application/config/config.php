14076d26e726d0cfef161fb61793096c1ffdc47b
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-26 08:25:20 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/parking/';
+$config['base_url'] = 'http://localhost/Parking-Management-System/';
 
 /*
 |--------------------------------------------------------------------------
